78e8f016dab3f265149d55b508231ed8b664ecfa
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2024-08-17 01:47:48 +0530

    added Controller

--- src/main/java/com/example/mockproject/EmpController.java
+++ src/main/java/com/example/mockproject/EmpController.java
@@ -1,13 +1,27 @@
 package com.example.mockproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmpController {
-
+	List<Employee> employees = new ArrayList<>();
+	
 	@GetMapping("employees")
-	public String getAllEmployees() {
-		return "";
+	public List<Employee> getAllEmployees() {
+		
+		return employees;
 	}
+	
+	@PostMapping("employees")
+	public String createEmployee(@RequestBody Employee employee) {
+		
+		employees.add(employee);
+		return "Saved Successfully...";
+	}	
 }
